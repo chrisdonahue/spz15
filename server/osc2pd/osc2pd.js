272a4656168213f97853a4c90b6f7862c0a19087
Christopher Donahue <christopherdonahue@gmail.com>
2015-04-09 01:30:04 +0000

    added connect disconnect messages

--- server/osc2pd/osc2pd.js
+++ server/osc2pd/osc2pd.js
@@ -44,22 +44,41 @@
 	 
 	// listen for web socket connections
 	web_socket_server.on('connection', function (socket) {
-		if (args.debug) {
-			console.log('new connection');
-		}
-
 		// create osc parser
 		var socket_osc = new osc.WebSocketPort({
 			socket: socket
 		});
+
+		// state for this user
+		var user = {
+			date_connected: (new Date()).toISOString(),
+			fingerprint: 0
+		}
 
 		// listen for messages on this socket, funnel to PD
 		socket_osc.on('message', function (message) {
 			if (args.debug) {
 				console.log(message);
 			}
+			if (message.address === '/connect') {
+				user.fingerprint = message.args[0];
+			}
 			if ('udp' in args) {
 				udp_client.send(message);
+			}
+		});
+
+		// send disconnect message
+		socket_osc.on('close', function () {
+			var disconnect = {
+				address: '/disconnect',
+				args: [user.fingerprint]
+			}
+			if (args.debug) {
+				console.log(disconnect);
+			}
+			if ('udp' in args) {
+				udp.client.send(disconnect);
 			}
 		});
 	});
